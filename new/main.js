fb2e7cb8d1d02b1e353d63f512970b1ceed3335c
Oliver <vuoreol@pm.me>
2023-04-16 11:41:48 +0000

    Changes

--- new/main.js
+++ new/main.js
@@ -1,7 +1,10 @@
 const socket = io();
+const constraints = { audio: true };
+let localStream = null;
 
-document.querySelector('#join').addEventListener('click', () => {
+document.querySelector('#join').addEventListener('click', async () => {
   const name = document.querySelector('#name').value;
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
   socket.emit('join', { name });
 });
 
@@ -10,4 +13,26 @@ socket.on('joined', data => {
   const participantDiv = document.createElement('div');
   participantDiv.innerText = name;
   document.querySelector('#participants').appendChild(participantDiv);
+  localStream.getTracks().forEach(track => {
+    const sender = peerConnection.addTrack(track, localStream);
+  });
+});
+
+const peerConnection = new RTCPeerConnection();
+
+peerConnection.addEventListener('track', event => {
+  const audioElement = document.createElement('audio');
+  audioElement.srcObject = event.streams[0];
+  document.querySelector('#participants').appendChild(audioElement);
+  const receiver = peerConnection.addTrack(event.track, localStream);
+  socket.emit('audio', { receiver });
+});
+
+socket.on('audio', data => {
+  const { receiver } = data;
+  peerConnection.getSenders().forEach(sender => {
+    if (sender.track.kind === 'audio') {
+      sender.replaceTrack(receiver);
+    }
+  });
 });
--- new/signaling.py
+++ new/signaling.py
@@ -3,16 +3,11 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/main.js")
-def main():
-    with open("main.js", "r") as f:
-        return f.read()    
 
 @socketio.on('connect')
 def handle_connect():
@@ -24,8 +19,18 @@ def handle_join(data):
 
 @socketio.on('audio')
 def handle_audio(data):
-    emit('audio', data, broadcast=True)
-
+    receiver = data['receiver']
+    @socketio.on('offer')
+    def handle_offer(offer):
+        peerConnection.setRemoteDescription(offer)
+        @socketio.on('answer')
+        def handle_answer(answer):
+            peerConnection.setLocalDescription(answer)
+            emit('answer', answer, to=receiver)
+    offer = peerConnection.createOffer()
+    peerConnection.setLocalDescription(offer)
+    emit('offer', offer, to=receiver)
 
 if __name__ == '__main__':
     socketio.run(app)
+
